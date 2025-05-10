4c7462f414cb027dbf4c2b07f5e5b6c1fcb0f2d5
Gagan Malvi <malvi@ersa.dev>
2025-05-10 21:08:31 +0530

    Add Omar Abdullah
    Signed-off-by: Gagan Malvi <malvi@ersa.dev>

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,8 @@ export default function WarRoomDashboard() {
     "ShashiTharoor",
     "narendramodi",
     "MEAIndia",
-    "rajnathsingh"
+    "rajnathsingh",
+    "OmarAbdullah"
   ];
 
   const speculativeTwitterHandles = [
